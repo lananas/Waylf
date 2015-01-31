e71dd79cb8988834b12e136da44f19463ac560fc
antoinerose <deft>
2015-01-31 16:30:36 +0100

    login+register sur bdd locale

---
+++ app/src/main/java/com/mushroom/waylf/Home.java
@@ -0,0 +1,18 @@
+package com.mushroom.waylf;
+
+/**
+ * Created by antoinerose on 31/01/2015.
+ */
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Home extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+    }
+}
+
