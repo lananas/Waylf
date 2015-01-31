e49ed6f30b5ba61026331e11255afc34bc594866
antoinerose <deft>
2015-01-31 20:21:50 +0100

    connection OMDb

---
+++ app/src/main/java/com/mushroom/waylf/Film.java
@@ -0,0 +1,17 @@
+package com.mushroom.waylf;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by antoinerose on 31/01/2015.
+ */
+public class Film extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.film);
+    }
+
+}
+
--- app/src/main/java/com/mushroom/waylf/Home.java
+++ app/src/main/java/com/mushroom/waylf/Home.java
@@ -4,15 +4,106 @@ package com.mushroom.waylf;
  * Created by antoinerose on 31/01/2015.
  */
 import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class Home extends Activity{
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class Home extends Activity implements View.OnClickListener {
+
+    private Button mFilm, mSerie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+
+        //setup buttons
+        mFilm = (Button)findViewById(R.id.film);
+        mSerie = (Button)findViewById(R.id.serie);
+
+        //register listeners
+        mFilm.setOnClickListener(this);
+        mSerie.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+        switch (v.getId()) {
+            case R.id.film:
+                //getInputStreamFromUrl("http://www.omdbapi.com/?t=*&y=&plot=short&r=json");
+                new RequestTask().execute("http://www.omdbapi.com/?t=iron+man&y=&plot=short&r=json");
+                Intent i = new Intent(this, Film.class);
+                startActivity(i);
+                break;
+            case R.id.serie:
+                //Intent j = new Intent(this, Serie.class);
+                //startActivity(j);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    class RequestTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... uri) {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpResponse response;
+            String responseString = null;
+            try {
+                response = httpclient.execute(new HttpGet(uri[0]));
+                StatusLine statusLine = response.getStatusLine();
+                if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    response.getEntity().writeTo(out);
+                    responseString = out.toString();
+                    Log.d("Login Successful!", out.toString());
+                    out.close();
+                } else{
+                    //Closes the connection.
+                    response.getEntity().getContent().close();
+                    throw new IOException(statusLine.getReasonPhrase());
+                }
+            } catch (ClientProtocolException e) {
+                //TODO Handle problems..
+            } catch (IOException e) {
+                //TODO Handle problems..
+            }
+            return responseString;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            //Do anything with response..
+        }
+    }
+
+
 }
 
