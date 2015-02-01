69804c820993a35825f0d459f4f7385ca57a189a
antoinerose <deft>
2015-02-01 15:41:53 +0100

    login+register sur bdd locale

--- app/src/main/java/com/mushroom/waylf/Home.java
+++ app/src/main/java/com/mushroom/waylf/Home.java
@@ -4,6 +4,7 @@ package com.mushroom.waylf;
  * Created by antoinerose on 31/01/2015.
  */
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -11,15 +12,20 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import com.mushroom.waylf.library.JSONParser;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
 import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
@@ -28,10 +34,22 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Home extends Activity implements View.OnClickListener {
 
+    // Progress Dialog
+    // JSON parser class
+    JSONParser jsonParser = new JSONParser();
+    private ProgressDialog pDialog;
     private Button mFilm, mSerie;
+
+    private static final String URL_Request = "http://www.omdbapi.com/?t=iron+man&y=&plot=short&r=json";
+
+    //JSON element ids from repsonse of php script:
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_MESSAGE = "message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,10 +71,10 @@ public class Home extends Activity implements View.OnClickListener {
         // TODO Auto-generated method stub
         switch (v.getId()) {
             case R.id.film:
-                //getInputStreamFromUrl("http://www.omdbapi.com/?t=*&y=&plot=short&r=json");
-                new RequestTask().execute("http://www.omdbapi.com/?t=iron+man&y=&plot=short&r=json");
-                Intent i = new Intent(this, Film.class);
-                startActivity(i);
+                //new RequestTask().execute("http://www.omdbapi.com/?t=iron+man&y=&plot=short&r=json");
+                new AttemptRequest().execute();
+                //Intent i = new Intent(this, Film.class);
+                //startActivity(i);
                 break;
             case R.id.serie:
                 //Intent j = new Intent(this, Serie.class);
@@ -67,7 +85,7 @@ public class Home extends Activity implements View.OnClickListener {
                 break;
         }
     }
-
+    /*
     class RequestTask extends AsyncTask<String, String, String> {
 
         @Override
@@ -102,6 +120,65 @@ public class Home extends Activity implements View.OnClickListener {
             super.onPostExecute(result);
             //Do anything with response..
         }
+    }
+    */
+    class AttemptRequest extends AsyncTask<String, String, String> {
+
+        /**
+         * Before starting background thread Show Progress Dialog
+         * */
+        boolean failure = false;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(Home.this);
+            pDialog.setMessage("Search...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... args) {
+            // TODO Auto-generated method stub
+            // Check for success tag
+            int success;
+
+            try {
+                // Building Parameters
+                List<NameValuePair> params = new ArrayList<NameValuePair>();
+
+                Log.d("request!", "starting");
+                // getting product details by making HTTP request
+                JSONObject json = jsonParser.makeHttpRequest(
+                        URL_Request, "GET", params);
+
+                // check your log for json response
+                Log.d("Request attempt", json.toString());
+
+                Log.d("Request Successful!", json.toString());
+                return json.getString(TAG_MESSAGE);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+
+        }
+        /**
+         * After completing background task Dismiss the progress dialog
+         * **/
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog once product deleted
+            pDialog.dismiss();
+            if (file_url != null){
+                Toast.makeText(Home.this, file_url, Toast.LENGTH_LONG).show();
+            }
+
+        }
+
     }
 
 
