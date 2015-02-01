366c28b80b98e40bbfba2c5bdb62541918adfb4e
lananas <anael.curatolo@gmail.com>
2015-02-01 19:21:15 +0100

    affichage liste des films et récupération de l'id

--- app/src/main/java/com/mushroom/waylf/MoviesList.java
+++ app/src/main/java/com/mushroom/waylf/MoviesList.java
@@ -1,7 +1,21 @@
 package com.mushroom.waylf;
 
 import android.app.Activity;
+import android.graphics.Movie;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.mushroom.waylf.library.Request;
+
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 /**
@@ -10,11 +24,70 @@ import android.os.Bundle;
 
 public class MoviesList extends Activity {
 
+    ListView liste = null;
+    TextView test = null;
+    List<String>  idMovie= null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         setContentView(R.layout.movies_list);
+
+        liste = (ListView) findViewById(R.id.listView);
+        test = (TextView) findViewById(R.id.textView3);
+        Request request = new Request();
+
+        // A changer
+        String json = "{\"Search\":[{\"Title\":\"Iron Man\",\"Year\":\"2008\",\"imdbID\":\"tt0371746\",\"Type\":\"movie\"},{\"Title\":\"Iron Man 3\",\"Year\":\"2013\",\"imdbID\":\"tt1300854\",\"Type\":\"movie\"}]}";
+        List<String> Movie = null;
+
+        try {
+            Movie = NewListMovie(request.ReadJsonList(json));
+            idMovie = NewIdList(request.ReadJsonList(json));
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Movie);
+            liste.setAdapter(adapter);
+            liste.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView,
+                                        View view,
+                                        int position,
+                                        long id) {
+                    //ouvrir fenetre avec changement envois de l'id
+                    //test.setText(idMovie.get(position));
+                    // Que faire quand on clique sur un élément de la liste ?
+
+                }
+            });
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
+
+    public List<String> NewListMovie(List<String> list)
+    {
+        List<String> Movie = new ArrayList<String>();
+        for(int i = 0; i<list.size();i++){
+            String delims = "[;]+";
+            String[] tokens = list.get(i).split(delims);
+            Movie.add(i,tokens[0] +" (" + tokens[1]+")");
+    }
+        return Movie;
+    }
+
+    public List<String> NewIdList(List<String> list)
+    {
+        List<String> Movie = new ArrayList<String>();
+        for(int i = 0; i<list.size();i++){
+            String delims = "[;]+";
+            String[] tokens = list.get(i).split(delims);
+            Movie.add(i,tokens[2]);
+        }
+        return Movie;
+    }
+
+
+
 }
 
