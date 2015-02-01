31f2e5675e47d91f9110b039e64b7820214c154b
lananas <anael.curatolo@gmail.com>
2015-02-01 17:49:11 +0100

    Création fonction parse json

--- app/src/main/java/com/mushroom/waylf/library/Request.java
+++ app/src/main/java/com/mushroom/waylf/library/Request.java
@@ -1,5 +1,19 @@
 package com.mushroom.waylf.library;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
 /**
  * Created by Mushroom on 01/02/2015.
  */
@@ -19,4 +33,27 @@ public class Request {
         query = "i=" + nameMovie + "&y=&plot=full&r=json";
         return omdbapiUrl + query;
     }
+
+    public JSONObject ReadJsonMovie(String JsonMovie) throws JSONException {
+        JSONObject json = null;
+        try {
+            json = (JSONObject)new JSONParser().parse(JsonMovie);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return json;
+    }
+
+    public List<String> ReadJsonList(String JsonList) throws JSONException {
+        JSONObject obj = new JSONObject(JsonList);
+        List<String> list = new ArrayList<String>();
+        JSONArray array = obj.getJSONArray("Search");
+        for(int i = 0 ; i < array.length() ; i++){
+            list.add(array.getJSONObject(i).getString("Title")+";"+array.getJSONObject(i).getString("Year") +";" + array.getJSONObject(i).getString("imdbID"));
+        }
+        return list;
+    }
+
+
+
 }
